0d64c8ead4160b934055c0d129c7dd6d325a14f9
MikeD <mike.d.hoore@telenet.be>
2017-04-06 11:45:13 +0200

    Oefening 2

--- src/be/vdab/oefenbundel/Oefenbundel.java
+++ src/be/vdab/oefenbundel/Oefenbundel.java
@@ -2,26 +2,29 @@ package be.vdab.oefenbundel;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Oefenbundel {
 
-	private static final String URL = "jdbc:mysql://localhost/bieren";
+	private static final String URL = "jdbc:mysql://localhost/bieren?useSSL=false";
 	private static final String USER = "cursist";
 	private static final String PASSWORD = "cursist";
-	private static final String UPDATE_SQL_TAAK1 = "DELETE FROM bieren WHERE alcohol IS NULL";
-	
+	private static final String SQL_TAAK2 ="SELECT brouwers.naam,count(*) AS aantal FROM brouwers INNER JOIN bieren ON brouwers.id=bieren.brouwerid GROUP BY brouwers.id, brouwers.naam ORDER BY brouwers.naam";
+			
 	public static void main(String[] args) {
 		
-		try (Connection connection =DriverManager.getConnection(URL,USER,PASSWORD); Statement statement = connection.createStatement()) {
+			try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(SQL_TAAK2)) {
 
-			System.out.println(statement.executeUpdate(UPDATE_SQL_TAAK1));
+			while (resultSet.next()) {
+			System.out.printf("%s %d%n", resultSet.getString("naam"), resultSet.getInt("aantal"));
+			}
 			
 			} catch (SQLException ex) {
-				
 			ex.printStackTrace();
-			
 			}
 	}
 	
