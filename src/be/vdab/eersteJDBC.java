3e2e291dce555ee9203f9678cea612064d04eeaf
MikeD <mike.d.hoore@telenet.be>
2017-04-06 12:03:21 +0200

    Eigen invoer met SQL

--- src/be/vdab/eersteJDBC.java
+++ src/be/vdab/eersteJDBC.java
@@ -4,36 +4,41 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Scanner;
+import java.sql.PreparedStatement;
 
 public class eersteJDBC {
 	
 	private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
 	private static final String USER = "cursist";
 	private static final String PASSWORD = "cursist";
-	private static final String SQL = "select id, naam, woonplaats from leveranciers where woonplaats = 'Wevelgem'";
+	private static final String SQL = "select id, naam, woonplaats from leveranciers where woonplaats = ?";
 	
 	public static void main(String[] args) {
 		
+		try( Scanner scanner = new Scanner(System.in)){
+			System.out.print("Woonplaats : ");
+			String woonplaats;
+			woonplaats = scanner.nextLine();
+		
+		
 		try (
 				Connection connection =DriverManager.getConnection(URL,USER,PASSWORD); 
-				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery(SQL)
+				PreparedStatement statement = connection.prepareStatement(SQL)
 				) {
 
-			int aantalLeveranciers = 0;
+			statement.setString(1, woonplaats);
+			try(ResultSet resultSet = statement.executeQuery()){
 			while (resultSet.next()){
-				++aantalLeveranciers;
-				System.out.printf("%4d %s%n", resultSet.getInt("id"), resultSet.getString("naam"));
+				System.out.println(resultSet.getString("naam"));
 			}
-			
-			System.out.printf("%d leverancier(s) van Wevelgem", aantalLeveranciers);
-			
+			}
 			} catch (SQLException ex) {
 				
 			ex.printStackTrace();
 			
 			}
+		}
 	}
 
 
