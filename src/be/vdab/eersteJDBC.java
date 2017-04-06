8b6c60eaf6dd8e394d5f485bdd618b4e4a7a203e
MikeD <mike.d.hoore@telenet.be>
2017-04-06 15:31:18 +0200

    Datum uit DB halen

--- src/be/vdab/eersteJDBC.java
+++ src/be/vdab/eersteJDBC.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Scanner;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class eersteJDBC {
@@ -13,30 +11,23 @@ public class eersteJDBC {
 	private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
 	private static final String USER = "cursist";
 	private static final String PASSWORD = "cursist";
-	private static final String SQL = "INSERT INTO soorten(naam) VALUES (?)";
+	private static final String SQL = "SELECT indienst, voornaam, familienaam FROM werknemers WHERE indienst >= {d '2001-1-1'} ORDER BY indienst";
 	
 	public static void main(String[] args) {		
 		
-		try( Scanner scanner = new Scanner(System.in)){
-			System.out.println("Geef soort namen, type STOP om de invoer te stoppen");
-			String naam = scanner.nextLine();
-		
 		try (
 			Connection connection = DriverManager.getConnection(URL,USER,PASSWORD); 
-			PreparedStatement statement = connection.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS)
-			) {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(SQL)
+			) {	
+			
+			while (resultSet.next()){
+				System.out.printf("%s %s %s%n", resultSet.getDate("indienst"), resultSet.getString("voornaam"), resultSet.getString("familienaam"));
+			}
 
-				statement.setString(1, naam);
-				statement.executeUpdate();
-				
-				try(ResultSet resultSet = statement.getGeneratedKeys()){
-					resultSet.next();
-					System.out.println(resultSet.getLong(1));
-				}
 
 			} catch (SQLException ex) {
 				ex.printStackTrace();
 			}
-		}
 	}
 }
