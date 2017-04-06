7a91a5c7a9291ce7aff0daa32a861cbbf6adcd42
MikeD <mike.d.hoore@telenet.be>
2017-04-06 13:58:08 +0200

    Meerdere insert met 1 itteratie

--- src/be/vdab/eersteJDBC.java
+++ src/be/vdab/eersteJDBC.java
@@ -1,9 +1,10 @@
 package be.vdab;
 
+import java.util.List;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.sql.PreparedStatement;
 
@@ -12,27 +13,27 @@ public class eersteJDBC {
 	private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
 	private static final String USER = "cursist";
 	private static final String PASSWORD = "cursist";
-	private static final String SQL = "select id, naam, woonplaats from leveranciers where woonplaats = ?";
+	private static final String SQL = "INSERT INTO soorten(naam) VALUES (?)";
 	
 	public static void main(String[] args) {
 		
-		try( Scanner scanner = new Scanner(System.in)){
-			System.out.print("Woonplaats : ");
-			String woonplaats;
-			woonplaats = scanner.nextLine();
+		List<String> namen = new ArrayList<>();
 		
+		try( Scanner scanner = new Scanner(System.in)){
+			System.out.println("Geef soort namen, type STOP om de invoer te stoppen");
+			for (String naam; ! "stop".equalsIgnoreCase(naam = scanner.nextLine()) ; namen.add(naam));
+		}
 		
 		try (
-				Connection connection =DriverManager.getConnection(URL,USER,PASSWORD); 
-				PreparedStatement statement = connection.prepareStatement(SQL)
-				) {
+			Connection connection = DriverManager.getConnection(URL,USER,PASSWORD); 
+			PreparedStatement statement = connection.prepareStatement(SQL)
+			) {
 
-			statement.setString(1, woonplaats);
-			try(ResultSet resultSet = statement.executeQuery()){
-			while (resultSet.next()){
-				System.out.println(resultSet.getString("naam"));
+			for(String naam : namen){
+				statement.setString(1, naam);
+				System.out.printf("%d %s%n", statement.executeUpdate(), naam);
 			}
-			}
+			
 			} catch (SQLException ex) {
 				
 			ex.printStackTrace();
@@ -40,6 +41,3 @@ public class eersteJDBC {
 			}
 		}
 	}
-
-
-}
