944fc2031b4d42afa8ad3fb818757b0f6bb44be4
MikeD <mike.d.hoore@telenet.be>
2017-04-06 09:31:28 +0200

    Oefening 1.1

---
+++ src/be/vdab/oefenbundel/Oefenbundel.java
@@ -0,0 +1,28 @@
+package be.vdab.oefenbundel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Oefenbundel {
+
+	private static final String URL = "jdbc:mysql://localhost/bieren";
+	private static final String USER = "cursist";
+	private static final String PASSWORD = "cursist";
+	private static final String UPDATE_SQL_TAAK1 = "DELETE FROM bieren WHERE alcohol = NULL";
+	
+	public static void main(String[] args) {
+		
+		try (Connection connection =DriverManager.getConnection(URL,USER,PASSWORD); Statement statement = connection.createStatement()) {
+
+			System.out.println(statement.executeUpdate(UPDATE_SQL_TAAK1));
+			
+			} catch (SQLException ex) {
+				
+			ex.printStackTrace();
+			
+			}
+	}
+	
+}
