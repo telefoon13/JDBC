d4ea9a410a9ba6358f8fb89db0bd261a78fc39c7
MikeD <mike.d.hoore@telenet.be>
2017-04-06 09:49:30 +0200

    kolommen aanduiden met hun volg nummer

--- src/be/vdab/eersteJDBC.java
+++ src/be/vdab/eersteJDBC.java
@@ -2,6 +2,7 @@ package be.vdab;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -10,13 +11,19 @@ public class eersteJDBC {
 	private static final String URL = "jdbc:mysql://localhost/tuincentrum";
 	private static final String USER = "cursist";
 	private static final String PASSWORD = "cursist";
-	private static final String UPDATE_SQL = "UPDATE planten SET verkoopprijs = verkoopprijs * 1.1";
+	private static final String SQL = "SELECT id,naam FROM leveranciers ORDER BY id";
 	
 	public static void main(String[] args) {
 		
-		try (Connection connection =DriverManager.getConnection(URL,USER,PASSWORD); Statement statement = connection.createStatement()) {
+		try (
+				Connection connection =DriverManager.getConnection(URL,USER,PASSWORD); 
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(SQL)
+				) {
 
-			System.out.println(statement.executeUpdate(UPDATE_SQL));
+			while (resultSet.next()){
+			System.out.printf("%4d %s%n", resultSet.getInt(1),resultSet.getString(2));
+			}
 			
 			} catch (SQLException ex) {
 				
