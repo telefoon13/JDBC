0d31c7ee08386029c44a7742694e89dd25c99edb
MikeD <mike.d.hoore@telenet.be>
2017-04-06 09:13:47 +0200

    eerste connectie met db

--- src/be/vdab/eersteJDBC.java
+++ src/be/vdab/eersteJDBC.java
@@ -1,5 +1,34 @@
 package be.vdab;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class eersteJDBC {
+	
+	private static final String URL = "jdbc:mysql://localhost/tuincentrum";
+	private static final String USER = "root";
+	private static final String PASSWORD = "vdab";
+	
+	public static void main(String[] args) {
+		
+		Connection connection = null;
+		
+		try {
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			System.out.println("Connectie gemaakt");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				try{
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
 
 }
