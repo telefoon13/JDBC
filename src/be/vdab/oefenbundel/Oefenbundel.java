a7610145aef84b21c0b59abc4903714069738bb8
MikeD <mike.d.hoore@telenet.be>
2017-04-10 09:54:29 +0200

    Oefening 4 : Bieren van de maand

--- src/be/vdab/oefenbundel/Oefenbundel.java
+++ src/be/vdab/oefenbundel/Oefenbundel.java
@@ -1,6 +1,5 @@
 package be.vdab.oefenbundel;
 
-import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -13,33 +12,32 @@ public class Oefenbundel {
 	private static final String URL = "jdbc:mysql://localhost/bieren?useSSL=false";
 	private static final String USER = "cursist";
 	private static final String PASSWORD = "cursist";
-	private static final String SQL_TAAK3 ="SELECT naam, alcohol FROM bieren WHERE alcohol BETWEEN ? AND ? ORDER BY alcohol, naam";
+	private static final String SQL_TAAK4 ="SELECT naam, verkochtsinds FROM bieren WHERE {fn month(verkochtsinds)} = ? ORDER BY verkochtsinds";
 			
 	public static void main(String[] args) {
 		
 		try (Scanner scanner = new Scanner(System.in)) {
-			System.out.print("Min :");
-			BigDecimal min = scanner.nextBigDecimal();
-			System.out.print("Max :");
-			BigDecimal max = scanner.nextBigDecimal();
+			System.out.print("Geef een maand nummer :");
+			int maand = scanner.nextInt();
 		
-			try (
+			if (maand >= 1 && maand < 13){
+				try (
 					Connection connection = DriverManager.getConnection(URL,USER,PASSWORD); 
-					PreparedStatement statement = connection.prepareStatement(SQL_TAAK3)
+					PreparedStatement statement = connection.prepareStatement(SQL_TAAK4)
 					) {
 				
-				statement.setBigDecimal(1, min);
-				statement.setBigDecimal(2, max);
+						statement.setInt(1, maand);
 				
-				try (ResultSet resultSet = statement.executeQuery()) {
-					while (resultSet.next()) {
-					System.out.println(resultSet.getString("naam") + "\t" + resultSet.getBigDecimal("alcohol"));
-					}
+							try (ResultSet resultSet = statement.executeQuery()) {
+								while (resultSet.next()) {
+									System.out.println(resultSet.getString("naam") + "\t" + resultSet.getDate("verkochtsinds"));
+								}
+							}
 				}
-		}
-			} catch (SQLException ex) {
-			ex.printStackTrace();
 			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
 	}
 	
 }
