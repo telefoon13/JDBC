97b04002589c31c0ac6983969a22a5726cfa104e
MikeD <mike.d.hoore@telenet.be>
2017-04-10 13:57:23 +0200

    Transaction met isolation level

--- src/be/vdab/eersteJDBC.java
+++ src/be/vdab/eersteJDBC.java
@@ -1,33 +1,59 @@
 package be.vdab;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Scanner;
 
 public class eersteJDBC {
 	
 	private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
 	private static final String USER = "cursist";
 	private static final String PASSWORD = "cursist";
-	private static final String SQL1 = "UPDATE planten SET verkoopprijs = verkoopprijs * 1.1 WHERE verkoopprijs >= 100";
-	private static final String SQL2 = "UPDATE planten SET verkoopprijs = verkoopprijs * 1.05 WHERE verkoopprijs < 100";
+	private static final String SQL1 = "SELECT verkoopprijs FROM planten WHERE id = ?";
+	private static final String SQL2 = "UPDATE planten SET verkoopprijs = ? WHERE id = ?";
 	
 	public static void main(String[] args) {	
 		
-		try (
-			Connection connection = DriverManager.getConnection(URL,USER,PASSWORD);
-			Statement statement = connection.createStatement()
-		) {	
+		try(Scanner scanner = new Scanner(System.in)){
+			System.out.print("ID : ");
+			int id = scanner.nextInt();
+			System.out.print("Prijs : ");
+			BigDecimal prijs = scanner.nextBigDecimal();
 			
-			connection.setAutoCommit(false);
-			statement.addBatch(SQL1);
-			statement.addBatch(SQL2);
-			statement.executeBatch();
-			connection.commit();
-
-		} catch (SQLException ex) {
-			ex.printStackTrace();
+			try(
+			Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			PreparedStatement statement1 = connection.prepareStatement(SQL1);
+			PreparedStatement statement2 = connection.prepareStatement(SQL2)
+			){
+				connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+				statement1.setInt(1, id);
+				connection.setAutoCommit(false);
+				
+				try(ResultSet resultSet = statement1.executeQuery()){
+					
+					if (resultSet.next()){
+						BigDecimal oudePrijs = resultSet.getBigDecimal("verkoopprijs");
+						
+						if (prijs.compareTo(oudePrijs.multiply(BigDecimal.valueOf(1.1))) <= 0 ){
+							statement2.setBigDecimal(1, prijs);
+							statement2.setInt(2, id);
+							statement2.executeUpdate();
+							connection.commit();
+						} else {
+							System.err.println("Nieuwe verkoopprijs is te hoog");
+						}
+					} else {
+						System.err.println("Plant niet gevonden");
+					}
+				}
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
