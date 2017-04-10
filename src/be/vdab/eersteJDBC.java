3ef0c41c6ca3f44e8f1cf7c0e91bbeb47daf8bdb
MikeD <mike.d.hoore@telenet.be>
2017-04-10 10:14:25 +0200

    Metdata over JDBC driver

--- src/be/vdab/eersteJDBC.java
+++ src/be/vdab/eersteJDBC.java
@@ -3,31 +3,23 @@ package be.vdab;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.ResultSet;
+import java.sql.DatabaseMetaData;
 
 public class eersteJDBC {
 	
 	private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
 	private static final String USER = "cursist";
 	private static final String PASSWORD = "cursist";
-	private static final String SQL = "SELECT geboorte, voornaam, familienaam FROM werknemers WHERE {fn month(geboorte)} = {fn month({fn curdate()})} ORDER BY {fn dayofmonth(geboorte)}";
 	
 	public static void main(String[] args) {		
 		
-		try (
-			Connection connection = DriverManager.getConnection(URL,USER,PASSWORD); 
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery(SQL)
-			) {	
+		try (Connection connection = DriverManager.getConnection(URL,USER,PASSWORD)) {	
 			
-			while (resultSet.next()){
-				System.out.printf("%s %s %s%n", resultSet.getDate("geboorte"), resultSet.getString("voornaam"), resultSet.getString("familienaam"));
-			}
-
-
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
+			DatabaseMetaData metaData = connection.getMetaData();
+			System.out.printf("%s %d.%d%n", metaData.getDriverName(), metaData.getDriverMajorVersion(), metaData.getDriverMinorVersion());
+			
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
 	}
 }
