1158fa06bf1beaf5ae1c6522b05734cb1ceb8946
MikeD <mike.d.hoore@telenet.be>
2017-04-10 12:16:37 +0200

    Oefening 6 : Falliete brouwer

--- src/be/vdab/oefenbundel/Oefenbundel.java
+++ src/be/vdab/oefenbundel/Oefenbundel.java
@@ -1,45 +1,35 @@
 package be.vdab.oefenbundel;
 
-import java.math.BigDecimal;
-import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
+import java.sql.Statement;
 
 public class Oefenbundel {
 
 	private static final String URL = "jdbc:mysql://localhost/bieren?useSSL=false&noAccessToProcedureBodies=true";
 	private static final String USER = "cursist";
 	private static final String PASSWORD = "cursist";
-	private static final String SQL_TAAK5 ="{call VanTotAlcohol2(?,?)}";
+	private static final String SQL_TAAK6a ="UPDATE bieren SET brouwerid = 2 WHERE brouwerid = 1 AND alcohol >= 8.5";
+	private static final String SQL_TAAK6b ="UPDATE bieren SET brouwerid = 3 WHERE brouwerid = 1 AND alcohol < 8.5";
+	private static final String SQL_TAAK6c ="DELETE FROM brouwers WHERE id = 1";
 			
-	public static void main(String[] args) {
+public static void main(String[] args) {	
 		
-		try (Scanner scanner = new Scanner(System.in)) {
-			System.out.print("Min :");
-			BigDecimal min = scanner.nextBigDecimal();
-			System.out.print("Max :");
-			BigDecimal max = scanner.nextBigDecimal();
-		
-			try (
-					Connection connection = DriverManager.getConnection(URL,USER,PASSWORD); 
-					CallableStatement statement = connection.prepareCall(SQL_TAAK5)
-					) {
-				
-				statement.setBigDecimal(1, min);
-				statement.setBigDecimal(2, max);
-				
-				try (ResultSet resultSet = statement.executeQuery()) {
-					while (resultSet.next()) {
-					System.out.println(resultSet.getString("naam") + "\t" + resultSet.getBigDecimal("alcohol"));
-					}
-				}
-		}
-			} catch (SQLException ex) {
+		try (
+			Connection connection = DriverManager.getConnection(URL,USER,PASSWORD);
+			Statement statement = connection.createStatement()
+		) {	
+			
+			connection.setAutoCommit(false);
+			statement.addBatch(SQL_TAAK6a);
+			statement.addBatch(SQL_TAAK6b);
+			statement.addBatch(SQL_TAAK6c);
+			statement.executeBatch();
+			connection.commit();
+
+		} catch (SQLException ex) {
 			ex.printStackTrace();
-			}
+		}
 	}
-	
 }
