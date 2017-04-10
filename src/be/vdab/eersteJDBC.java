e178176e83995cef68874f8808b606aef0ccaefc
MikeD <mike.d.hoore@telenet.be>
2017-04-10 12:03:56 +0200

    Meerdere SQL met Transaction

--- src/be/vdab/eersteJDBC.java
+++ src/be/vdab/eersteJDBC.java
@@ -1,47 +1,30 @@
 package be.vdab;
 
-import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
+import java.sql.Statement;
 
 public class eersteJDBC {
 	
-	private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false&noAccessToProcedureBodies=true";
+	private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
 	private static final String USER = "cursist";
 	private static final String PASSWORD = "cursist";
-	private static final String CALL = "{call PlantenMetEenWoord(?)}";
+	private static final String SQL1 = "UPDATE planten SET verkoopprijs = verkoopprijs * 1.1 WHERE verkoopprijs >= 100";
+	private static final String SQL2 = "UPDATE planten SET verkoopprijs = verkoopprijs * 1.05 WHERE verkoopprijs < 100";
 	
 	public static void main(String[] args) {	
 		
-		try (Scanner scanner = new Scanner(System.in)) {
-			System.out.print("Geef een woord :");
-			String woord = scanner.nextLine();
-		
-			try (
-				Connection connection = DriverManager.getConnection(URL,USER,PASSWORD);
-				CallableStatement statement = connection.prepareCall(CALL)
-				) {	
-				
-				statement.setString(1, '%' + woord + '%');
+		try (
+			Connection connection = DriverManager.getConnection(URL,USER,PASSWORD);
+			Statement statement = connection.createStatement()
+		) {	
 			
-				try( ResultSet resultSet = statement.executeQuery()){
-					int resultaten = 0;
-					
-					while(resultSet.next()){
-						System.out.println(resultSet.getString("naam"));
-						resultaten = 1;
-					}
-					
-					if (resultaten == 0){
-						System.out.println("Geen resultaten");
-					}
-				
-				
-			}
-		}
+			connection.setAutoCommit(false);
+			statement.executeUpdate(SQL1);
+			statement.executeUpdate(SQL2);
+			connection.commit();
+
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
