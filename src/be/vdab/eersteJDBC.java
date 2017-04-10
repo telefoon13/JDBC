a4c808eed35ef832d4f7659e4e796a84847cefad
MikeD <mike.d.hoore@telenet.be>
2017-04-10 11:11:57 +0200

    Stored procedures uitvoeren

--- src/be/vdab/eersteJDBC.java
+++ src/be/vdab/eersteJDBC.java
@@ -1,33 +1,47 @@
 package be.vdab;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Scanner;
 
 public class eersteJDBC {
 	
-	private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
+	private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false&noAccessToProcedureBodies=true";
 	private static final String USER = "cursist";
 	private static final String PASSWORD = "cursist";
-	private static final String SQL = "SELECT id, voornaam, indienst FROM werknemers";
+	private static final String CALL = "{call PlantenMetEenWoord(?)}";
 	
-	public static void main(String[] args) {		
+	public static void main(String[] args) {	
 		
-		try (
-			Connection connection = DriverManager.getConnection(URL,USER,PASSWORD);
-			Statement statement = connection.createStatement()
-			) {	
-			
-			try( ResultSet resultSet = statement.executeQuery(SQL)){
-				ResultSetMetaData metaData = resultSet.getMetaData();
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.print("Geef een woord :");
+			String woord = scanner.nextLine();
+		
+			try (
+				Connection connection = DriverManager.getConnection(URL,USER,PASSWORD);
+				CallableStatement statement = connection.prepareCall(CALL)
+				) {	
 				
-				for( int index = 1 ; index <= metaData.getColumnCount() ; index++){
-					System.out.printf("%s %s%n", metaData.getColumnName(index), metaData.getColumnTypeName(index));
-				}
+				statement.setString(1, '%' + woord + '%');
+			
+				try( ResultSet resultSet = statement.executeQuery()){
+					int resultaten = 0;
+					
+					while(resultSet.next()){
+						System.out.println(resultSet.getString("naam"));
+						resultaten = 1;
+					}
+					
+					if (resultaten == 0){
+						System.out.println("Geen resultaten");
+					}
+				
+				
 			}
+		}
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
